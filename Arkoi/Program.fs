2df45edd9c79aca344910ef59dd061e1a1ab1de6
Gage Herrmann <g@gageh.us>
2015-05-27 13:29:17 -0500

    Added temporary game initialization code

--- Arkoi/Program.fs
+++ Arkoi/Program.fs
@@ -1,6 +1,31 @@
-﻿open Game
+﻿open Arkoi.Game
+open Arkoi.Types
+
+let map1 = [ 
+        "##############";
+        "#>           #          ######";
+        "#            ############    #";
+        "#            -          +    #";
+        "#    ~~      ############    #";
+        "#     ~~     #          #    #";
+        "#      ~~    #          # <  #";
+        "##############          ######" ]
+
+let tokenizeMap map = [for row in map -> [for column in row -> column]]
+
+let generateCoordinates =
+    let mutable y' = 0
+    let mutable coord = List.empty
+    for row in tokenizeMap map1 do
+        let mutable x' = 0
+        for col in row do
+            coord <- List.append coord [{ coordinate = (x', y'); image = col; }]
+            x' <- x' + 1
+        y' <- y' + 1
+    coord
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let state = {tiles = generateCoordinates; protagonist = { position = (4,4); facing = Right }}
+    stepGame state
     0 // return an integer exit code
