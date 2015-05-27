dcd20d23295719e0212f45d54ca9baa273b65e58
Gage Herrmann <g@gageh.us>
2015-05-27 13:42:21 -0500

    Readded handling of excape key

--- Arkoi/Game.fs
+++ Arkoi/Game.fs
@@ -8,19 +8,21 @@ module Game =
     let rec getInput() = 
         let input = Console.ReadKey(true) 
         match input.Key with
-            | ConsoleKey.W ->            Move(Up)
-            | ConsoleKey.D ->            Move(Right)
-            | ConsoleKey.S ->            Move(Down)
-            | ConsoleKey.A ->            Move(Left)
-            | ConsoleKey.UpArrow ->      Face(Up)
-            | ConsoleKey.RightArrow ->   Face(Right)
-            | ConsoleKey.DownArrow ->    Face(Down)
-            | ConsoleKey.LeftArrow ->    Face(Left)
-            | ConsoleKey.X ->            Attack
-            | ConsoleKey.E ->            Interact
+            | ConsoleKey.W ->           Move(Up)
+            | ConsoleKey.D ->           Move(Right)
+            | ConsoleKey.S ->           Move(Down)
+            | ConsoleKey.A ->           Move(Left)
+            | ConsoleKey.UpArrow ->     Face(Up)
+            | ConsoleKey.RightArrow ->  Face(Right)
+            | ConsoleKey.DownArrow ->   Face(Down)
+            | ConsoleKey.LeftArrow ->   Face(Left)
+            | ConsoleKey.X ->           Attack
+            | ConsoleKey.E ->           Interact
+            | ConsoleKey.Escape ->      Exit
             | _ -> getInput() // If user pressed a key we don't want to handle, ask for input again
 
     let rec stepGame state =
+        printfn "%A" state
         //Graphics.render state
         match getInput() with
         | Move direction -> state |> stepGame
